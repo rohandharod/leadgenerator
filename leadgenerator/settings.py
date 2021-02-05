236f8674029bc8dc2a85862165ab83107ca0bc80
Rohan <rohandharod2001@gmail.com>
2021-02-05 14:22:05 +0530

    Master created

--- leadgenerator/settings.py
+++ leadgenerator/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'master.apps.MasterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
